9e4d31ad9b098cc5038cec89432600d9b510e1d3
Peter Lind <ppl_peter@protonmail.com>
2020-04-11 19:24:54 +0200

    new file:   python/.ipynb_checkpoints/BlackScholes-hedging-checkpoint.ipynb 	new file:   python/.ipynb_checkpoints/European call option exampel-checkpoint.ipynb 	new file:   python/.ipynb_checkpoints/explicit finite difference-checkpoint.ipynb 	new file:   python/BlackScholes-hedging.ipynb 	new file:   python/European call option exampel.ipynb 	new file:   python/GPRMCCV/.ipynb_checkpoints/GPR-MC-CV-checkpoint.ipynb 	new file:   python/GPRMCCV/.ipynb_checkpoints/Untitled-checkpoint.ipynb 	new file:   python/GPRMCCV/GPR-MC-CV.ipynb 	new file:   python/GPRMCCV/Untitled.ipynb 	new file:   python/Spyder/closedEuro.py 	new file:   python/deephedge.pdf 	new file:   python/explicit finite difference.ipynb

---
+++ python/Spyder/closedEuro.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 10 22:04:05 2020
+"Closed form European put"
+@author: ppl
+"""
+
+import numpy as np
+from scipy import stats
+from scipy.stats import norm
+
+#analytical formula for european call:
+def priceECall(t,s, sigma, K, r, T):
+    d1 = (1/(sigma*(T-t)**(0.5))) * (np.log(s/K) + (r + sigma**2 / 2) * (T-t))
+    d2 = d1 - sigma * (T-t)**0.5
+    return s* norm.cdf(d1) - np.exp(-r*(T-t))*K*norm.cdf(d2)
+
+#Put-call parity proposition 9.2 in Björk
+def priceEPut(t,s, sigma, K, r, T):
+    return (K*np.exp(-r*(T-t)) + priceECall(t,s, sigma, K, r, T) - s)
+
+print(priceEPut(0,36,0.2,40,0.06,1))
+#nice it worked
+
+#Present result nicely with pandas
+import pandas as pd
+from pandas import Series, DataFrame
+
+stockValues = np.linspace(36,44,5)
+sigma = np.linspace(.2,.4,2)
+maturity= np.linspace(1,2,2)
+
+
+dict1= {'SpotPrice': [], 'Volatility': [], 
+                   'Time to maturity': [], 'Closed form European' : []}
+for s in stockValues:
+    for volatility in sigma:
+        for T in maturity:
+            dict1['SpotPrice'].append(s)
+            dict1['Volatility'].append(volatility)
+            dict1['Time to maturity'].append(T)
+            dict1['Closed form European'].append(priceEPut(0,s,volatility,40,0.06,T))
+            
+print(DataFrame(dict1))
